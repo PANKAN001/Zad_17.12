e1f3019158965e1cec6b64701d360c922e2d25ab
PANIKAN001 <95694958+PANKAN001@users.noreply.github.com>
2021-12-17 12:05:46 +0300

    Add

---
+++ Zad_17.12_2/Zad_17.12_2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zad_17._12_2
+{
+    public partial class Form1 : Form
+    {
+        Point moveStart; // точка для перемещения
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.BackColor = Color.Yellow;
+            Button button1 = new Button
+            {
+                Location = new Point
+                {
+                    X = this.Width / 3,
+                    Y = this.Height / 3
+                }
+            };
+            button1.Text = "Закрыть";
+            button1.Click += button1_Click;
+            this.Controls.Add(button1); // добавляем кнопку на форму
+            this.Load += Form1_Load;
+            this.MouseDown += Form1_MouseDown;
+            this.MouseMove += Form1_MouseMove;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button helloButton = new Button();
+            helloButton.BackColor = Color.LightGray;
+            helloButton.ForeColor = Color.DarkGray;
+            helloButton.Location = new Point(10, 10);
+            helloButton.Text = "Привет";
+            this.Controls.Add(helloButton);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // если нажата левая кнопка мыши
+            if (e.Button == MouseButtons.Left)
+            {
+                moveStart = new Point(e.X, e.Y);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // если нажата левая кнопка мыши
+            if ((e.Button & MouseButtons.Left) != 0)
+            {
+                // получаем новую точку положения формы
+                Point deltaPos = new Point(e.X - moveStart.X, e.Y - moveStart.Y);
+                // устанавливаем положение формы
+                this.Location = new Point(this.Location.X + deltaPos.X,
+                  this.Location.Y + deltaPos.Y);
+            }
+        }
+    }
+}
